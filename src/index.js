96a8d71b619aa6a7191581639892159484ca16a0
myungho.lee <myungho.lee@brique.co.kr>
2021-06-01 08:30:50 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.log("test");
+    };
+    return App;
+}());
+exports.App = App;
+var app = new App();
+app.test();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export class App {
+  test() {
+    console.log("test10");
+  }
+}
+
+const app = new App();
+app.test();
